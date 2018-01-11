9282f124ff79a5eaa8349a31f7bb0aa0709ec440
jheers <jheers@lavision.de>
2018-01-11 15:41:55 +0100

    refactor

--- makeCircle/makeCircle/makeCircle.cpp
+++ makeCircle/makeCircle/makeCircle.cpp
@@ -1,7 +1,8 @@
 #include <math.h>
 #include "mex.h"
+#include <string>
 
-namespace jh
+namespace JH
 {
 	struct Point
 	{
@@ -23,31 +24,38 @@ namespace jh
 		Point P;
 	};
 }
-void checkargs(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
+bool checkargs(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
 {
-	if (nrhs < 5)  mexErrMsgIdAndTxt("MATLAB:makeCircle", "Needs five arguments");
-	if (!mxIsNumeric(prhs[0])) mexErrMsgIdAndTxt("MATLAB:makeCircle", "Invalid arg 1: Must be numeric");
-	if (!mxIsNumeric(prhs[1])) mexErrMsgIdAndTxt("MATLAB:makeCircle", "Invalid arg 2: Must be numeric");
-	if (!mxIsNumeric(prhs[2])) mexErrMsgIdAndTxt("MATLAB:makeCircle", "Invalid arg 3: Must be numeric");
-	if (!mxIsNumeric(prhs[3])) mexErrMsgIdAndTxt("MATLAB:makeCircle", "Invalid arg 4: Must be numeric");
-	if (!mxIsNumeric(prhs[4])) mexErrMsgIdAndTxt("MATLAB:makeCircle", "Invalid arg 5: Must be numeric");
+	if (nrhs == 0)
+	{
+		mexPrintf("Usage A = %s(rows,cols,centerX,centerY,radius)\n", mexFunctionName()); return false;
+	}
+	if (nrhs < 5)  
+		mexErrMsgIdAndTxt("MATLAB:makeCircle", "Needs five arguments");
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::string tag("MATLAB:"); tag += mexFunctionName();
+		std::string msg("Invalid argument "); msg += std::to_string(i+1)+": Must be numeric!";
+		if (!mxIsNumeric(prhs[i])) mexErrMsgIdAndTxt(tag.c_str(), msg.c_str());
+	}
+	return true;
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
 {
-	checkargs(nlhs, plhs, nrhs, prhs);
+	if (!checkargs(nlhs, plhs, nrhs, prhs)) return;
 	// Create boolean 2D array
 	mwSize dims[2] = { mwSize(mxGetPr(prhs[0])[0]), mwSize(mxGetPr(prhs[1])[0]) };
 	plhs[0] = mxCreateNumericArray(2, dims, mxLOGICAL_CLASS, mxREAL);
 	// Compile calculation data
-	jh::Point center{ mxGetPr(prhs[2])[0] - 1, mxGetPr(prhs[3])[0] - 1 };
+	JH::Point center{ mxGetPr(prhs[2])[0] - 1, mxGetPr(prhs[3])[0] - 1 };
 	double radius = mxGetPr(prhs[4])[0];
 	bool *data = (bool*)mxGetData(plhs[0]);
-	jh::Circle circle(center, radius);
+	JH::Circle circle(center, radius);
 	// Calculate make values by location
 	for (auto i = 0; i <  dims[0] * dims[1]; i++)
 	{
-		jh::Point P{ double(i % dims[0]), double(i / dims[0]) };
+		JH::Point P{ double(i % dims[0]), double(i / dims[0]) };
 		data[i] = circle.includes(P);
 	}
 }
